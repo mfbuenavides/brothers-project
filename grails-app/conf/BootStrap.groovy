3221b770b8cfb07b8f88ad728984f113f4855337
glennbrian <glennscythe@gmail.com>
2014-07-08 15:20:47 +0800

    add useraccount sample data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,15 @@
 class BootStrap {
 
     def init = { servletContext ->
+    	def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+
+	    def testUser = new Account(username: 'admin', enabled: true, password: 'admin')
+	    testUser.save(flush: true)
+
+	    AccountRole.create testUser, adminRole, true
+
+	    assert Account.count() == 1
+	    assert AccountRole.count() == 1
     }
     def destroy = {
     }
