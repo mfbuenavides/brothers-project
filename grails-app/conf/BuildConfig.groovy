d4fb5c5298ffbcad132ab61a9ced294f11e450f3
mfbuenavides <mfbuenavides@gmail.com>
2014-07-29 16:53:37 +0800

    edited src/templates/scaffolding. generated new view pages

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -47,7 +47,7 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.8.3"
-        runtime ":resources:1.1.6"
+        //runtime ":resources:1.1.6"
 		runtime ":foundation:5.2.2"
 		
         // Uncomment these (or add new ones) to enable additional resources capabilities
