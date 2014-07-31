d537ba3c586420eed9e22ea127b3ab224b37085f
antibry <glennscythe@gmail.com>
2014-07-31 13:44:17 +0800

    edit Raiser domain class, modify getFullName method

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -24,8 +24,19 @@ environments {
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = 'update' // for mock testing purposes only
+            jndiName = "java:comp/env/jdbc/tbasdb"
+            pooled = true
+            logSql = false
+            properties {
+                 minEvictableIdleTimeMillis = 1800000
+                 timeBetweenEvictionRunsMillis = 1800000
+                 numTestsPerEvictionRun = 3
+                 testOnBorrow = true
+                 testWhileIdle = true
+                 testOnReturn = true
+                 validationQuery = "SELECT 1"
+            }
         }
     }
     production {
